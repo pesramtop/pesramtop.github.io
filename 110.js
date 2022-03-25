e52c90f75a9faf25bad9825e9e907abd31ad0e6b
@faridiot <dev@designer.web.id>
2022-03-25 20:18:27 +0700

    setItem

--- 110.js
+++ 110.js
@@ -3,10 +3,10 @@ var btn = document.getElementById("110");
 function myFunction() {
   if (btn.innerHTML=="Yes! saya sudah hafal") btn.innerHTML = "Saya belum hafal";
   else btn.innerHTML = "Yes! saya sudah hafal";
-  window.localStorage.setItem('btnLabel', btn.innerHTML);
+  window.localStorage.setItem('110', btn.innerHTML);
 }
 
-var btnLabel = window.localStorage.getItem('btnLabel');
+var btnLabel = window.localStorage.getItem('110');
 if (btnLabel) {
   btn.innerHTML = btnLabel;
 }
--- 111.js
+++ 111.js
@@ -3,10 +3,10 @@ var btn = document.getElementById("111");
 function myFunction() {
   if (btn.innerHTML=="Yes! saya sudah hafal") btn.innerHTML = "Saya belum hafal";
   else btn.innerHTML = "Yes! saya sudah hafal";
-  window.localStorage.setItem('btnLabel', btn.innerHTML);
+  window.localStorage.setItem('111', btn.innerHTML);
 }
 
-var btnLabel = window.localStorage.getItem('btnLabel');
+var btnLabel = window.localStorage.getItem('111');
 if (btnLabel) {
   btn.innerHTML = btnLabel;
 }
